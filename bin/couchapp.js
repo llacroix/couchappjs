8ba18fc8f03d5c99febcb2569ba2f721fbf26a63
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-02 14:41:27 +0300

    Added simple plugin lookup and removed uncessary things

--- bin/couchapp.js
+++ bin/couchapp.js
@@ -8,29 +8,11 @@ var yargs = require('yargs')
 
   , couchapp = require("../index.js")
 
+// Load command line scripts
+couchapp.lookupPlugins()
 couchapp.registerPlugin(require("../lib/cmd/init"))
 couchapp.registerPlugin(require("../lib/cmd/sync"))
 
-if(!couchapp.execute(argv._[0], yargs))
+if (!couchapp.execute(argv._[0], yargs)) {
   yargs.showHelp()
-
-/*
-function addCommand(commands, commandName) {
-  commands[commandName] = require("../lib/cmd/" + commandName)
 }
-
-function runCommand(commands) {
-  var commandName = argv._[0]
-
-  if (commands[commandName]) {
-    commands[commandName](yargs)
-  } else {
-    yargs.showHelp()
-  }
-}
-
-addCommand(commands, "sync")
-addCommand(commands, "init")
-
-runCommand(commands)
-*/
